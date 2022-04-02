d772c2fe14a03840c105bf040fe6f60c7eb709ee
tonwija <tonwija@gmail.com>
2022-04-02 14:07:23 +0000

    add explaination

--- Data_Warehouse/Table_optimization.sql
+++ Data_Warehouse/Table_optimization.sql
@@ -1,9 +1,11 @@
+--CREATE EXTERNAL TABLE FROM DATALAKE/GCS BUCKET WITH PARQUET FILE FORMAT
 CREATE OR REPLACE EXTERNAL TABLE `zoomcamp-de-339202.trips_data_all.external_biketrip`
 OPTIONS (
   format = 'parquet',
   uris = ['gs://dtc_data_lake_zoomcamp-de-339202/raw/2020*.parquet']
 );
 
+--CREATE TABLE NON EXTERNAL FROM EXTERNAL TABLE FOR OPTIMIZATION
 CREATE OR REPLACE TABLE `zoomcamp-de-339202.trips_data_all.biketrip_nonpartitoned` AS
 SELECT * FROM `zoomcamp-de-339202.trips_data_all.external_biketrip`;
 
